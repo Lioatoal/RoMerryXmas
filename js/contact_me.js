b1beb31a93ee46f0655f9217b06dcd0496b8d153
Royalor <roy20423@hotmail.com>
2016-12-23 09:36:50 +0800

    Update contact_me.js
    Modify main function's structure

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,19 +1,15 @@
 $(function() {
-
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
+    $("#contactForm").submit( function(event){
             // get values from FORM
             var name = $("input#name").val();
 
-            alert("hello word");
-        }
+            if (name == "roy") 
+            { 
+                alert("hello word"); 
+                $("#contact").hide();
+            } else {
+                alert("no this man");
+            }
+            event.preventDefault();
     });
 });
